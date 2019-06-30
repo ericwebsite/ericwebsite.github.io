c6f703728a9a0f8b7d895c3027df7887d0cec887
denny <yinglong_w@hotmail.com>
2019-06-30 13:58:30 -0700

    Fixed wrong format for input numbers

--- src/python/Translator.py
+++ src/python/Translator.py
@@ -1,8 +1,9 @@
 #Ask for binamy or not.
-A = input("Is the number binamy(b) or not((other key))")
-B = input("What is the number(say 0b before a binamy number)?")
-if A == "b":
-    C = int(B)
+A = input("Translate the number to binary(b) or not((other key))")
+B = input("What is the number?")
+
+if A != "b":
+    C = int(B, 2)
 else:
-    C = bin(B)
-print("{} in {} is {}.",format(B,A,C))
+    C = bin(int(B, 10))
+print("{} in {} is {}.".format(B,A,C))
